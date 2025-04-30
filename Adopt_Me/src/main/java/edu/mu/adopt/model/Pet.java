2d504f2af69417fa0b35b937d9b65467e419b5d8
BanTheMan <brandonmgomes06@gmail.com>
2025-04-30 18:38:02 -0500

    Pet class skeleton

--- Adopt_Me/src/main/java/edu/mu/adopt/model/Pet.java
+++ Adopt_Me/src/main/java/edu/mu/adopt/model/Pet.java
@@ -3,13 +3,66 @@ package edu.mu.adopt.model;
 /**
  * MVC Model
  */
-public abstract class Pet implements Comparable<Pet>{
-
+public abstract class Pet implements Comparable<Pet> {
 	
-	// Implement comparator methods
+    private Integer id;
+    private String name;
+    private String type;
+    private String species;
+    private Integer age;
+    private boolean adopted;
+    
+    public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public boolean isAdopted() {
+		return adopted;
+	}
+
+	public void setAdopted(boolean adopted) {
+		this.adopted = adopted;
+	}
+	
 	@Override
 	public int compareTo(Pet o) {
-		// TODO Auto-generated method stub
+		// Compare name
 		return 0;
 	}
 }
