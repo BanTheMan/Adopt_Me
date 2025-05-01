145965810f023eb36d119015ff04cccd45fd8b5c
IvanR314 <Ivanrhodes15@gmail.com>
2025-05-01 00:09:58 -0500

    Partial main implementation.

--- Adopt_Me/src/main/java/edu/mu/adopt/Main.java
+++ Adopt_Me/src/main/java/edu/mu/adopt/Main.java
@@ -1,10 +1,35 @@
 package edu.mu.adopt;
 
+import edu.mu.adopt.model.ExoticAnimal;
+import edu.mu.adopt.model.ExoticAnimalAdapter;
+import edu.mu.adopt.model.Pet;
+import edu.mu.adopt.model.Shelter;
+import edu.mu.adopt.utility.HandleJSON;
+import edu.mu.adopt;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		List<Pet> pets = HandleJSON.loadpet();
+		List<ExoticAnimal> epets = HandleJSON.loadexoticanimal();
+		
+		List<Pet> allpets = new ArrayList<>(pets);
+		
+		for(ExoticAnimal exotic: epets) 
+		{
+			allpets.add(new ExoticAnimalAdapter(exotic));
+		}
+		
+		Shelter<Pet> shelter = new Shelter<>();
+		for(Pet pet: allpets) 
+		{
+			shelter.addPet(pet);
+		}
+		
+		
 	}
 
 }
