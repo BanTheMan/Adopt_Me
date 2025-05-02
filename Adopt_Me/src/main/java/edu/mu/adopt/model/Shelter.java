919afc7865b3ea3672cb6891df2053d4a225a54b
BanTheMan <brandonmgomes06@gmail.com>
2025-05-02 18:11:34 -0500

    Completed Shelter model container class

--- Adopt_Me/src/main/java/edu/mu/adopt/model/Shelter.java
+++ Adopt_Me/src/main/java/edu/mu/adopt/model/Shelter.java
@@ -1,54 +1,114 @@
 package edu.mu.adopt.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
 
 /**
  * MVC Container Model
  */
 public class Shelter<T extends Pet> {
 	
-	private List<T> adoptablePets;
-	private List<T> unadoptablePets;
+	private Set<T> inStock;
+	private Set<T> importable;
 	
 	public Shelter() {
-		this.adoptablePets = new ArrayList<>();
+		this.inStock = new HashSet<T>();
+		this.importable = new HashSet<T>();
 	}
 	
-	public List<T> getAdoptablePets() {
-		return adoptablePets;
+	public Set<T> getPetsInStock() {
+		return inStock;
 	}
 
-	public void setAdoptablePets(List<T> adoptablePets) {
-		this.adoptablePets = adoptablePets;
+	public void setPetsInStock(Set<T> adoptablePets) {
+		this.inStock = adoptablePets;
 	}
 
-	public List<T> getUnadoptablePets() {
-		return unadoptablePets;
+	public Set<T> getImportablePets() {
+		return importable;
 	}
 
-	public void setUnadoptablePets(List<T> unadoptablePets) {
-		this.unadoptablePets = unadoptablePets;
+	public void setImportablePets(Set<T> unadoptablePets) {
+		this.importable = unadoptablePets;
 	}
 	
-	public void addPet(T pet) {
-		
+//	public T getPetInStock(T pet) {
+//		for (T petInStock : inStock) {
+//			if (petInStock.equals(pet)) {
+//				return petInStock;
+//			}
+//		}
+//		return null;
+//	}
+	
+	public void addPetToStock(T pet) {
+		this.inStock.add(pet);
 	}
 	
-	public boolean removePet(Integer id) {
+	public void removePetFromStock(T badPet) {
+		for (T pet : inStock) {
+			if (pet.equals(badPet)) {
+				inStock.remove(badPet);
+			}
+		}
+	}
+	
+//	public T getImportablePet(T pet) {
+//		for (T importable : importable) {
+//			if (importable.equals(pet)) {
+//				return importable;
+//			}
+//		}
+//		return null;
+//	}
+	
+	public void addImportablePet(T pet) {
+		this.importable.add(pet);
+	}
+	
+	public void removeImportablePet(T pet) {
+		for (T terriblePet : importable) {
+			if (terriblePet.equals(pet)) {
+				importable.remove(terriblePet);
+			}
+		}
+	}
+	
+	public boolean isInStock(T pet) {
+		if (inStock.contains(pet)) {
+			return true;
+		}
 		return false;
 	}
 	
-	public void getDetails() { // toString?
-		
-	}
-
-	public void adoptPet(T pet) {
-		
+	public boolean isImportable(T pet) {
+		if (importable.contains(pet)) {
+			return true;
+		}
+		return false;
 	}
 	
-	public void importExoticPet(T pet) {
-		
+	/**
+	 * Add pet into stock from importable set
+	 * @param redeemedPet
+	 */
+	public void addPet(T redeemedPet) {
+		// If pet is importable, add to stock
+		if (isImportable(redeemedPet)) {
+			removeImportablePet(redeemedPet);
+			addPetToStock(redeemedPet);
+		}
+		// TODO: add fail message: Pet is not importable
+	}
+	
+	public boolean removePet(T badPet) {
+		if (isInStock(badPet)) {
+			removePetFromStock(badPet);
+			addImportablePet(badPet);
+		}
+		// TODO: add fail message: Pet not in stock or not in shelter
+		return false;
 	}
 
 }
