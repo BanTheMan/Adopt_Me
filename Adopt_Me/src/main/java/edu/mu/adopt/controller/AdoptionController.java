4ddc5d24093cfc64c8d5b60a409c7a575872dc21
BanTheMan <brandonmgomes06@gmail.com>
2025-05-02 18:11:50 -0500

    Completed controller skeleton

--- Adopt_Me/src/main/java/edu/mu/adopt/controller/AdoptionController.java
+++ Adopt_Me/src/main/java/edu/mu/adopt/controller/AdoptionController.java
@@ -1,5 +1,46 @@
 package edu.mu.adopt.controller;
 
+import edu.mu.adopt.model.Home;
+import edu.mu.adopt.model.Pet;
+import edu.mu.adopt.model.Shelter;
+import edu.mu.adopt.view.AdoptionView;
+
 public class AdoptionController {
+	
+	private Shelter shelter;
+	private Home home;
+	private AdoptionView view;
+	
+	public AdoptionController() {
+		this.shelter = new Shelter<>();
+		this.view = new AdoptionView();
+		this.home = new Home<Pet>();
+	}
+	
+	public <T extends Pet> void addButtonPressed(T pet) {
+		
+	}
+	
+	public <T extends Pet> void importExoticPet(T pet) {
+		
+	}
+	
+	public <T extends Pet> void adoptPetButtonPressed(T pet) {
+		// Remove pet from shelter
+		// Place pet in home
+		
+	}
+	
+	public <T extends Pet> void removePetButtonPressed(T pet) {
+		
+	}
+	
+	public <T extends Pet> void viewDetailsButtonPressed(T pet) {
+		
+	}
+	
+	public <T extends Pet> void saveButtonPressed(T pet) {
+		
+	}
 
 }
