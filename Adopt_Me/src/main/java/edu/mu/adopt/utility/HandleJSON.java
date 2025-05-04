e8350415f4276d699dfd2f863ec824577bf669f5
BanTheMan <brandonmgomes06@gmail.com>
2025-05-03 21:08:08 -0500

    Commented file paths

--- Adopt_Me/src/main/java/edu/mu/adopt/utility/HandleJSON.java
+++ Adopt_Me/src/main/java/edu/mu/adopt/utility/HandleJSON.java
@@ -14,6 +14,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 
+// Adopt_Me\src\main\resources\pets.json
+// Adopt_Me\src\main\resources\exotic_animals.json
+
 /**
  * Class for loading and saving JSON files
  */
